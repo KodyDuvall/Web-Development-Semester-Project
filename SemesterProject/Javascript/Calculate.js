66799e48cb39f442a1c7a03488cc17342d92335b
KodyDuvall <109024935+KodyDuvall@users.noreply.github.com>
2022-10-15 14:31:55 -0500

    Add files via upload

--- SemesterProject/Javascript/Calculate.js
+++ SemesterProject/Javascript/Calculate.js
@@ -1,27 +1,32 @@
 document.getElementById('ButtonID').addEventListener('click', ButtonClick);
-var buttonSound = new Audio("audio/ButtonSound.wav");
+//var buttonSound = new Audio("audio/ButtonSound.wav");
 
 let userInput = "";
 let tempString = "";
 let isCorrectFormat;
 let periodCount;//Because Javascript thinks things like 1.3.1.41.45 = 1.3  ?????
 let entries = [];
+let modeInformationArray = [];
 let sum;
 let mean;
 let variance;
 let mode;
+let modeOccurrenceNum;
 let standardDeviation;
+let median;
 
 function ButtonClick(){
     window.localStorage.clear();
-    buttonSound.play();
+    //buttonSound.play(); I couldn't get this working properly. I will if I can another time.
     
     sum = 0;
     mean = 0;
     variance = 0;
     standardDeviation = 0;
     periodCount = 0;
+    median = 0;
     mode = "";
+    modeOccurrenceNum = "";
     tempString = "";
     entries.splice(0, entries.length);//This removes all entries from the array
     isCorrectFormat = true;
@@ -102,23 +107,21 @@ function ButtonClick(){
             standardDeviation = Math.sqrt(variance);
         }
 
-        /*The commented code below was some debugging code. I am keeping it in just in case I need it again.
-        alert("Sum: " + sum + " Mean: " + mean + " variance: " + variance + " Standard Deviation: " + standardDeviation);
-        
-        let arrayEles = "";
-        for(let i = 0; i < entries.length; i++){
-            arrayEles += "Entry # " + (i + 1) + ": " + entries[i] + " "
-        }
+        median = FindMedian(entries);
 
-        alert(arrayEles);
-        */
+        modeInformationArray = DetermineModes(entries);
+        mode = modeInformationArray[0];
+        modeOccurrenceNum = modeInformationArray[1];
 
         //usage of localStorage seems necessary in order for data to be transferred amongest different web pages
         //localStorage uses a Key-Value pair system, like the dictionary data structure
         localStorage.setItem("SUM", sum);
+        localStorage.setItem("MEDIAN", median); 
         localStorage.setItem("MEAN", mean);
-        //localStorage.setItem("MODE", mode); //I haven't written the mode function yet
+        localStorage.setItem("MODE", mode); 
+        localStorage.setItem("MODENUM", modeOccurrenceNum); 
         localStorage.setItem("VARIANCE", variance);
+        localStorage.setItem("ENTRIESCOUNT", entries.length);
         localStorage.setItem("STANDARDDEVIATION", standardDeviation);
         
         self.location = "calculationPage.html";//this is one way to open another page. self is called because the next webpage is locally found in a folder
@@ -127,7 +130,7 @@ function ButtonClick(){
     else{//this else block handles what happens when one or more entries is not valid
         alert("Incorrect format");
         /*I want to do something else here other than just inform the user that the contents of the textarea is not in the
-          right format using a pop-up message. I might want to make the text box red and as well or something.*/
+          right format using a pop-up message. I might want to make the text box red as well or something like that.*/
     }
 }
 
@@ -159,3 +162,96 @@ function CalculateVariance(mean, array, arrayLength){
     //Note that arrayLength is a necessary parameter here due to the distinction between the sample/population formulas
 }
 
+/*
+    Below is the function that determines the mode(s) of the entries. My code here is probably very inefficient. 
+    I just could not think of another way for the program to determine the mode(s) of the entries. If you know of 
+    a better way of finding the mode(s), please rewrite it as you see fit!
+*/
+
+function DetermineModes(entriesArray){
+    let count;
+    let currentElement;
+    let maximumCount;
+    let modesString = "";
+    let countArray = [];
+    let countParallelArray = [];
+    let resultantArray = []; //because I needed to return more than one variable. This might be considered bad practice, not sure lol
+
+    entriesArray.sort(function(a, b){return b - a});
+
+    for(let i = 0; i < entriesArray.length; i++){
+        console.log("Entry #" + (i + 1) + ": " + entriesArray[i]);
+    }
+
+    for(let i = 0; i < entriesArray.length; i++){
+        currentElement = entriesArray[i];
+        count = 0;
+
+        if(currentElement === entriesArray[i + 1]){//this is here to save time on useless checks
+            for(let I = i; I < entriesArray.length; I++){
+                if(currentElement === entriesArray[I]){
+                    count++;
+                    i++;
+    
+                    if(I === entriesArray.length - 1){ //to ensure last element is captured
+                        countArray.push(count);
+                        countParallelArray.push(currentElement);
+                        break;    
+                    }
+                }
+    
+                else{
+                    countArray.push(count);
+                    countParallelArray.push(currentElement);
+                    i--;
+                    break;
+                }
+            }
+        }
+    }
+
+    maximumCount = 1;
+
+    for(let i = 0; i < countArray.length; i++){//find max
+        if(countArray[i] > maximumCount){
+            maximumCount = countArray[i];
+        }
+    }
+
+    if(maximumCount === 1){
+        resultantArray[0] = "No value has a frequency greater than one.";
+        resultantArray[1] = maximumCount.toString();
+
+        return resultantArray;
+    }
+
+    else{
+        for(let i = 0; i < countArray.length; i++){
+            if(countArray[i] === maximumCount){
+                modesString += countParallelArray[i].toString() + ", ";
+            }
+        }
+
+        modesString = modesString.slice(0, -1);
+        modesString = modesString.slice(0, -1);
+        resultantArray[0] = modesString;
+        resultantArray[1] = maximumCount.toString();
+
+        return resultantArray;
+    }
+
+    
+}
+
+//I tried to get a little clever with this function. I might start writing more code like this to keep the files shorter.
+function FindMedian(entriesArray){
+    entriesArray.sort(function(a, b){return b - a});
+
+    if(entriesArray.length % 2 === 1){
+        return (entriesArray[(Math.ceil(entriesArray.length / 2) - 1)]);
+    }
+
+    else{
+        return ((entriesArray[(entriesArray.length / 2) - 1] + entriesArray[(entriesArray.length / 2)]) / 2);
+    }
+}
